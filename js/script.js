5444f81e2be64852fdd0370fbc5d1bacc33ff6a7
Shawn Saras <shawnsaras@gmail.com>
2016-05-24 19:59:02 -0700

    setup annotation canvas - starting work to be able to draw

--- js/script.js
+++ js/script.js
@@ -2,8 +2,31 @@
  * script.js
  */
 
+var canvas = document.getElementById('annotationCanvas');
+var context = canvas.getContext('2d');
+var radius = 10;
+var circle = Math.PI * 2;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function putPoint(e) {
+	var xOffset = e.clientX + document.documentElement.scrollLeft;
+	var yOffset = e.clientY + document.documentElement.scrollTop;
+	
+	console.log(e);
+	console.log("offset X: " + e.offsetX);
+	console.log("offset Y: " + e.offsetY);
+	
+	context.beginPath();
+	context.arc(xOffset, yOffset, radius, 0, circle, false);
+	context.fill();
+}
+
+canvas.addEventListener('mousedown', putPoint);
 
 $(document).ready(function() {
+	console.log("jquery loaded");
     console.log("script.js started");
 
     // $("#print").click(function() {
