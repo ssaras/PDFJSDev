27577df0e48d7016c2fa1ee7443c85878a66924b
Shawn Saras <shawnsaras@gmail.com>
2016-05-24 20:34:27 -0700

    drawing starting to work

--- js/script.js
+++ js/script.js
@@ -4,35 +4,38 @@
 
 var canvas = document.getElementById('annotationCanvas');
 var context = canvas.getContext('2d');
-var radius = 10;
-var circle = Math.PI * 2;
-
+var drawing = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function putPoint(e) {
-	var xOffset = e.clientX + document.documentElement.scrollLeft;
-	var yOffset = e.clientY + document.documentElement.scrollTop;
-	
-	console.log(e);
-	console.log("offset X: " + e.offsetX);
-	console.log("offset Y: " + e.offsetY);
-	
-	context.beginPath();
-	context.arc(xOffset, yOffset, radius, 0, circle, false);
-	context.fill();
+function putPoint(e) {	
+	if(drawing) {
+		var x = e.clientX;
+		var y = e.clientY;
+		var radius = 10;
+		var startAngle = 0;
+		var endAngle = Math.PI * 2;
+		var antiClockwise = false;
+		
+		console.log(e);
+		console.log("offset X: " + e.offsetX);
+		console.log("offset Y: " + e.offsetY);
+		
+		context.beginPath();
+		context.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+		context.fill();
+	}
 }
 
-canvas.addEventListener('mousedown', putPoint);
+canvas.addEventListener('mouseup', function() {
+	drawing = false;
+});
+canvas.addEventListener('mousedown', function() {
+	drawing = true;
+});
+canvas.addEventListener('mousemove', putPoint);
 
 $(document).ready(function() {
 	console.log("jquery loaded");
     console.log("script.js started");
-
-    // $("#print").click(function() {
-    //     alert("hi");
-    //     $(".mainNav").hide();
-    //     $(".fileExplorer").hide();
-    // });
-    
 });
