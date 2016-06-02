249b82092539f2cfc0fddd433b9e454d145a12cc
Shawn Saras <shawnsaras@gmail.com>
2016-06-02 10:02:38 -0700

    canvas scrolling with pdf working

--- js/script.js
+++ js/script.js
@@ -8,50 +8,51 @@ var drawing = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function putPoint(e) {	
-	// if(drawing) {
+function putPoint(e) {
 		
-		var boundX = e.target.getBoundingClientRect().top;
-		var boundY = e.target.getBoundingClientRect().left
+	var bx = e.target.getBoundingClientRect().top;
+	var by = e.target.getBoundingClientRect().left
 
-		var x = e.offsetX + boundX;
-		var y =   460;//e.offsetY; //- boundY;
+	var cx = e.clientX;
+	var cy = e.clientY;
 
-		var radius = 5;
-		var startAngle = 0;
-		var endAngle = Math.PI * 2;
-		var antiClockwise = false;
-		
-		console.log(e);
-		console.log(e.target.getBoundingClientRect());		
-		console.log('');
-		console.log("Bound X: " + boundX);
-		console.log("Bound Y: " + boundY);
-		console.log('');
-		console.log("offset X: " + e.offsetX);
-		console.log("offset Y: " + e.offsetY);
-		console.log('');
-		console.log("client X: " + e.clientX);
-		console.log("client Y: " + e.clientY);
-		console.log('');
-		console.log('x: ' + x);
-		console.log('y: ' + y);
-		
-		context.beginPath();
-		context.arc(x, y, radius, startAngle, endAngle, antiClockwise);
-		context.fill();
-	// }
+	var ox = e.offsetX;
+	var oy = e.offsetY;
+
+	var x = ox;//e.offsetX + bx;
+	//460
+	var y = oy;//e.offsetY; //- by;
+
+	var radius = 5;
+	var startAngle = 0;
+	var endAngle = Math.PI * 2;
+	var antiClockwise = false;
+	
+	console.log(e);
+	console.log(e.target.getBoundingClientRect());		
+	console.log('');
+	console.log("Bound X: " + bx);
+	console.log("Bound Y: " + by);
+	console.log('');
+	console.log("offset X: " + ox);
+	console.log("offset Y: " + oy);
+	console.log('');
+	console.log("client X: " + cx);
+	console.log("client Y: " + cy);
+	console.log('');
+	console.log('x: ' + x);
+	console.log('y: ' + y);
+	
+	context.beginPath();
+	context.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+	context.fill();
 }
-
-// canvas.addEventListener('mouseup', function() {
-// 	drawing = false;
-// });
-// canvas.addEventListener('mousedown', function() {
-// 	drawing = true;
-// });
 canvas.addEventListener('mousedown', putPoint);
 
 $(document).ready(function() {
-	// console.log("jquery loaded");
- //    console.log("script.js started");
+	var viewer = $('#viewer');
+	console.log(viewer);
+	viewer.each(function() {
+		console.log($(this));
+	});
 });
