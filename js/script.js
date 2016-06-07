2ec1a1822d81844a3087887de8dfc034da9212a8
Shawn Saras <shawnsaras@gmail.com>
2016-06-07 16:49:24 -0700

    free draw, circles rectangles and lines working.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,26 @@
  * script.js
  */
 
- var canvas;
- var context;
- var rect, circle, isDown, origX, origY;
+var canvas,
+	context,
+	rect,
+	circle,
+	line,
+	free, 
+	origX,
+	origY,	
+	isMouseDown = false,
+	drawState = "LINE"; //RECT, CIRC, LINE, FREE
+
+
 
 function putPoint(e) {
-	var x = e.offsetX;;
-	var y = e.offsetY;
-
-	var radius = 5;
-	var startAngle = 0;
-	var endAngle = Math.PI * 2;
-	var antiClockwise = false;
+	var x = e.offsetX,
+		y = e.offsetY,
+		radius = 5,
+		startAngle = 0,
+		endAngle = Math.PI * 2,
+		antiClockwise = false;
 	
 	console.log(e);
 	console.log(e.target.getBoundingClientRect());
@@ -25,8 +33,134 @@ function putPoint(e) {
 	context.fill();
 }
 
-function drawRect() {
-	 // body...  
+function initRect(o) {	
+	var pointer = canvas.getPointer(o.e);
+	isMouseDown = true;
+	origX = pointer.x;
+	origY = pointer.y;
+	rect = new fabric.Rect({
+		left: pointer.x,
+		top: pointer.y,
+		strokeWidth: 3,
+		stroke: 'red',
+		selectable: true,
+		opacity : 0.5
+  	});
+  	canvas.add(rect);
+}
+
+function drawRect(o){
+	if (!isMouseDown) return;
+	var pointer = canvas.getPointer(o.e);
+	rect.set({ 
+		width: Math.abs(origX - pointer.x),
+		height: Math.abs(origY - pointer.y)
+	});
+	canvas.renderAll();
+}
+
+function initCirc(o) {	
+	var pointer = canvas.getPointer(o.e);
+	isMouseDown = true;
+	origX = pointer.x;
+	origY = pointer.y;
+	circle = new fabric.Circle({
+		left: pointer.x,
+		top: pointer.y,
+		radius: 1,
+		strokeWidth: 5,
+		stroke: 'red',
+		selectable: false,
+		originX: 'center', originY: 'center',
+		opacity : 0.5
+	});
+	canvas.add(circle);
+}
+
+function drawCirc(o) {
+	if (!isMouseDown) return;
+	var pointer = canvas.getPointer(o.e);
+	circle.set({ radius: Math.abs(origX - pointer.x) });
+	canvas.renderAll();
+}
+
+function initLine(o) {	
+	var pointer = canvas.getPointer(o.e);
+	isMouseDown = true;
+	origX = pointer.x;
+	origY = pointer.y;
+	rect = new fabric.Rect({
+		left: pointer.x,
+		top: pointer.y,
+		strokeWidth: 3,
+		stroke: 'red',
+		selectable: true,
+		opacity : 1
+  	});
+  	canvas.add(rect);
+}
+
+function drawLine(o){
+	if (!isMouseDown) return;
+	var pointer = canvas.getPointer(o.e);
+	rect.set({ 
+		width: Math.abs(origX - pointer.x),
+		height: 1
+	});
+	canvas.renderAll();
+}
+
+function initFree (o) {
+	 canvas.isDrawingMode = true;
+}
+
+function canvasSave() {
+	canvas.isDrawingMode = false;
+	cdc = JSON.stringify(canvas);
+	console.log(cdc);
+}
+
+function canvasRedraw() {
+	canvas.loadFromJSON(cdc);
+	canvas.renderAll();
+}
+
+function initDrawState (o) {
+	canvas.isDrawingMode = false;
+	switch (drawState) {
+		case "RECT":
+			initRect(o);
+			break;
+		case "CIRC":
+			initCirc(o);
+			break;
+		case "LINE":
+			initLine(o);
+			break;
+		case "FREE":
+			initFree(o);
+			break;
+		default:
+			alert("Please Select a Drawing Method")
+			break;
+	}
+}
+
+function drawDrawState (o) {
+	switch (drawState) {
+		case "RECT":
+			drawRect(o);
+			break;
+		case "CIRC":
+			drawCirc(o);
+			break;
+		case "LINE":
+			drawLine(o);
+			break;
+		default:
+			// statements_def
+			break;
+	}
 }
 
 function initializeAnnotationCanvas() {
@@ -42,9 +176,9 @@ function initializeAnnotationCanvas() {
 
 	var canvasSave = document.getElementById('save-mode'),
 		canvasClear = document.getElementById('clear-mode'),
-	    canvasRedraw = document.getElementById('redraw-mode');
-	    canvasDrawRect = document.getElementById('draw-rect');
-
+	    canvasRedraw = document.getElementById('redraw-mode'),
+	    canvasSetDrawState = $('.setDrawState');
+	    
 	var cdc = ''; // store canvas data	
 
 	canvas = new fabric.Canvas('annotationCanvas', { selection: false });
@@ -55,53 +189,32 @@ function initializeAnnotationCanvas() {
 	});
 
 	canvas.on('mouse:down', function(o){
-		isDown = true;
-		var pointer = canvas.getPointer(o.e);
-		origX = pointer.x;
-		origY = pointer.y;
-		console.log(origX);
-		console.log(origY);
-		rect = new fabric.Rect({
-			left: pointer.x,
-			top: pointer.y,
-			strokeWidth: 3,
-			stroke: 'red',
-			selectable: true,
-			opacity : 0.5
-	  	});
-	  	canvas.add(rect);
+		initDrawState(o);
 	});
 
 	canvas.on('mouse:move', function(o){
-		if (!isDown) return;
-		var pointer = canvas.getPointer(o.e);
-		rect.set({ 
-			width: Math.abs(origX - pointer.x),
-			height: Math.abs(origY - pointer.y)
-		});
-		canvas.renderAll();
+		drawDrawState(o);
 	});
 
 	canvas.on('mouse:up', function(o){
-		isDown = false;
+		isMouseDown = false;
 	});
 
 	// ------------------------------------------ //
 			
 	// canvas.isDrawingMode = true;
 	canvasSave.onclick = function() {
-		canvas.isDrawingMode = false;
-		cdc = JSON.stringify(canvas);
-		console.log(cdc);
+		canvasSave();
 	};
 	canvasClear.onclick = function() {
 		canvas.clear();
 	};
 	canvasRedraw.onclick = function() {
-		canvas.loadFromJSON(cdc);
-		canvas.renderAll();
+		canvasRedraw();
 	};
-
-
+	canvasSetDrawState.click(function(e) {
+		drawState = $(this).attr('drawState');
+		console.log(drawState);
+	});
 }
 
